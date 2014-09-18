2d8ae0bd962c4f5733f6f08b32dff40462108644
cmsdaq <cmsroma.daq@gmail.com>
2014-09-18 18:15:33 +0200

    Added VX718 interface

---
+++ src/CAEN_VX718.cpp
@@ -0,0 +1,42 @@
+#include "interface/CAEN_VX718.hpp"
+#include <iostream>
+
+int CAEN_VX718::Init()
+{
+}
+
+int CAEN_VX718::Clear()
+{
+}
+
+int CAEN_VX718::BufferClear()
+{
+}
+
+int CAEN_VX718::Config(BoardConfig *bC)
+{
+}
+
+int CAEN_VX718::Read(vector<WORD> &v)
+{
+}
+
+int CAEN_VX718::SetHandle(vector<WORD> &v)
+{
+}
+
+int CAEN_VX718::TriggerReceived()
+{
+}
+
+int CAEN_VX718::SendSignal(VX718_DAQ_Signals)
+{
+}
+
+int CAEN_VX718::PrintConfiguration()
+{
+}
+
+int CAEN_VX718::ParseConfiguration()
+{
+}
