b9d68dd2aa3c9a4b09ce0ec6a117e736659e538b
cmsdaq <cmsdaq@pcethtb2.cern.ch>
2014-10-02 11:06:03 +0200

    Modified ParseData to accept non-GUI specific commands from GTK dummy GUI

--- src/Daemon.cpp
+++ src/Daemon.cpp
@@ -135,7 +135,79 @@ Command Daemon::ParseData(dataType &mex)
 		ostringstream s; s<<"[Daemon]::[ParseCommand]::[DEBUG] GUI Mex: '"<< (char*)mex2.data()<<"'";
 		Log(s.str(),3);
 #endif
-		if (N >=12  and !strcmp( (char*) mex2.data(), "GUI_STARTRUN")  )
+		ostringstream s; s<<"[Daemon]::[ParseCommand]::[DEBUG] GUI Mex: '"<< (char*)mex2.data()<<"'";
+		Log(s.str(),3);
+
+	if (N >=4  and !strcmp( (char*) mex.data(), "SEND")  )
+		//	((char*)mex.data() ) [0]== 'S' and
+		//	((char*)mex.data() ) [1]== 'E' and
+		//	((char*)mex.data() ) [2]== 'N' and
+		//	((char*)mex.data() ) [3]== 'D' and
+		//	((char*)mex.data() ) [4]== '\0'  
+		{
+		//mex.erase(dataTypeSize_t(0),dataTypeSize_t(5));
+		mex.erase(0,5);
+		myCmd.cmd=SEND;
+		myCmd.data = mex.data();
+		myCmd.N    = mex.size();
+		mex.release();
+		}
+	else if (N >=4  and !strcmp( (char*) mex.data(), "RECV")  )
+		{
+		mex.erase(0,5);
+		myCmd.cmd=RECV;
+		myCmd.data = mex.data();
+		myCmd.N    = mex.size();
+		mex.release();
+		}
+	else if (N >=4  and !strcmp( (char*) mex.data(), "DATA")  )
+		{
+		mex.erase(0,5);
+		myCmd.cmd=DATA;
+		myCmd.data = mex.data();
+		myCmd.N    = mex.size();
+		mex.release();
+		}
+	else if (N >=3  and !strcmp( (char*) mex.data(), "NOP")  )
+		myCmd.cmd=NOP;
+	else if (N >=3  and !strcmp( (char*) mex.data(), "WWE")  )
+		myCmd.cmd=WWE;
+	else if (N >=2  and !strcmp( (char*) mex.data(), "WE")  )
+		myCmd.cmd=WE;
+	else if (N >=2  and !strcmp( (char*) mex.data(), "EE")  )
+		myCmd.cmd=EE;
+	else if (N >=3  and !strcmp( (char*) mex.data(), "WBT")  )
+		myCmd.cmd=WBT;
+	else if (N >=3  and !strcmp( (char*) mex.data(), "WBE")  )
+		myCmd.cmd=WBE;
+	else if (N >=3  and !strcmp( (char*) mex.data(), "EBT")  )
+		myCmd.cmd=EBT;
+	//	else if (N >=7  and !strcmp( (char*) mex.data(), "STATUS")  ) // move in the not null terminated part TODO
+	//		{
+	//		mex.erase(0,7);
+	//		myCmd.cmd=STATUS;
+	//		myCmd.data = mex.data();
+	//		myCmd.N    = mex.size();
+	//		mex.release();
+	//		}
+	else if (N >=8  and !strcmp( (char*) mex.data(), "STARTRUN")  )
+		{
+		mex.erase(0,9);
+		myCmd.cmd=STARTRUN;
+		myCmd.data = mex.data();
+		myCmd.N    = mex.size();
+		mex.release();
+		}
+	else if (N >=10  and !strcmp( (char*) mex.data(), "SPILLCOMPL")  )
+		myCmd.cmd=SPILLCOMPL;
+	else if (N >=13  and !strcmp( (char*) mex.data(), "EB_SPILLCOMPL")  )
+		myCmd.cmd=EB_SPILLCOMPLETED;
+	else if (N >=6  and !strcmp( (char*) mex.data(), "ENDRUN")  )
+		myCmd.cmd=ENDRUN;
+	else if (N >=3  and !strcmp( (char*) mex.data(), "DIE")  )
+		myCmd.cmd=DIE;
+	// GUI CMD are not NULL Terminated
+	else if (N >=12  and !strcmp( (char*) mex2.data(), "GUI_STARTRUN")  )
 		   {
 		   mex2.erase(0,13);
 		   myCmd.cmd=GUI_STARTRUN;
@@ -156,18 +228,10 @@ Command Daemon::ParseData(dataType &mex)
 		   {
 		   myCmd.cmd=GUI_RESTARTRUN;
 		   }
-		else if (N >=8  and !strcmp( (char*) mex2.data(), "GUI_DIE")  )
+		else if (N >=7  and !strcmp( (char*) mex2.data(), "GUI_DIE")  )
 		   {
 		   myCmd.cmd=GUI_DIE;
 		   }
-		else if (N >=7  and !strcmp( (char*) mex2.data(), "STATUS")  ) // here because this is spaced and not null/null terminated
-			{
-			mex2.erase(0,7);
-			myCmd.cmd=STATUS;
-			myCmd.data = mex2.data();
-			myCmd.N    = mex2.size();
-			mex2.release();
-			}
 		if (myCmd.data != NULL)mex2.release();
 		} // ENDGUI
 
