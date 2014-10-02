ab4dae81b789db91f3ed61141c65fc63c604de73
cmsromadaq <cmsdaq@pcethtb3.cern.ch>
2014-10-02 19:52:39 +0200

    removed -d from startall

--- scripts/startall.sh
+++ scripts/startall.sh
@@ -14,7 +14,7 @@ mycommand=" mkdir -p DAQ ; cd DAQ ; [ -d H4DAQ ] || git clone git@github.com:cms
 
 for machine in pcethtb1 cms-h4-03 ; do 
 	
-	mydataro="cd DAQ/H4DAQ; ./bin/datareadout -d -c data/config_${machine}_DR.xml -v ${verbosity} -l /tmp/log_h4daq_datareadout_\$(date +%s).log  > /tmp/log_h4daq_start_dr_${machine}_\$(date +%s).log" 
+	mydataro="cd DAQ/H4DAQ; nohup ./bin/datareadout  -c data/config_${machine}_DR.xml -v ${verbosity} -l /tmp/log_h4daq_datareadout_\$(date +%s).log  > /tmp/log_h4daq_start_dr_${machine}_\$(date +%s).log" 
 	
 	[ "${dryrun}" == "0" ] || {  echo "$mycommand" ; echo "$mydataro" ; continue; }
 	[ "${start_dr}" == "0" ] && continue;
@@ -30,7 +30,7 @@ done
 
 for machine in pcethtb2 ; do 
 
-	myrc="cd DAQ/H4DAQ; nohup ./bin/runcontrol  -d -c data/config_${machine}_RC.xml -v ${verbosity} -l /tmp/log_h4daq_runcontrol_\$(date +%s).log >  /tmp/log_h4daq_start_rc_${machine}_\$(date +%s).log " 
+	myrc="cd DAQ/H4DAQ; nohup ./bin/runcontrol  -c data/config_${machine}_RC.xml -v ${verbosity} -l /tmp/log_h4daq_runcontrol_\$(date +%s).log >  /tmp/log_h4daq_start_rc_${machine}_\$(date +%s).log " 
 	[ "${dryrun}" == "0" ] || {  echo "$mycommand" ; echo "$mydatarc" ; continue; }
 	[ "${start_rc}" == "0" ] && continue;
 	## compile
@@ -45,7 +45,7 @@ done
 
 for machine in pcethtb2 ; do 
 
-	myeb="cd DAQ/H4DAQ; nohup ./bin/eventbuilder  -d -c data/config_${machine}_EB.xml -v ${verbosity} -l /tmp/log_h4daq_eventbuilder_\$(date +%s).log >  /tmp/log_h4daq_start_eb_${machine}_\$(date +%s).log " 
+	myeb="cd DAQ/H4DAQ; nohup ./bin/eventbuilder   -c data/config_${machine}_EB.xml -v ${verbosity} -l /tmp/log_h4daq_eventbuilder_\$(date +%s).log >  /tmp/log_h4daq_start_eb_${machine}_\$(date +%s).log " 
 	[ "${dryrun}" == "0" ] || {  echo "$mycommand" ; echo "$mydatarc" ; continue; }
 	[ "${start_eb}" == "0" ] && continue;
 	## compile
