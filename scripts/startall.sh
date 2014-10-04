eead0342ed268ad7270bdd92dc9d66dd7921ad90
Paolo Meridiani <paolo.meridiani@cern.ch>
2014-10-04 16:49:29 +0200

    Adding options to startall

--- scripts/startall.sh
+++ scripts/startall.sh
@@ -7,11 +7,15 @@ verbosity=3
 #start_rc=1
 #start_eb=0
 
+daquser="cmsdaq"
+daqhome="/home/cmsdaq/DAQ/H4DAQ"
+norecompile=0
+logdir="/tmp"
 dr=""
 rc=""
 eb=""
 
-TEMP=`getopt -o dv: --long verbose:,dr:,eb:,rc:,dryrun -n 'startall.sh' -- "$@"`
+TEMP=`getopt -o dv: --long verbose:,logdir:,daquser:,daqhome:,dr:,eb:,rc:,norecompile,dryrun -n 'startall.sh' -- "$@"`
 
 if [ $? != 0 ] ; then echo "Options are wrong..." >&2 ; exit 1 ; fi
 
@@ -23,6 +27,13 @@ while true; do
   case "$1" in
     -v | --verbose ) verbosity="$2"; shift 2 ;;
     -d | --dryrun ) dryrun=1; shift;;
+    --norecompile ) norecompile=1; shift;;
+    --daquser )
+      daquser="$2"; shift 2 ;;
+    --daqhome )
+      daqhome="$2"; shift 2 ;;
+    --logdir )
+      logdir="$2"; shift 2 ;;
     --dr )
       dr="$2"; shift 2 ;;
     --rc )
@@ -34,59 +45,61 @@ while true; do
   esac
 done
 
-echo "========================================"
+echo "=================================================================="
+echo "Starting H4DAQ installed @${daqhome} as ${daquser}"
 [ "${dr}" == "" ] || echo "DR MACHINES: ${dr}"
 [ "${rc}" == "" ] || echo "RC MACHINES: ${rc}"
 [ "${eb}" == "" ] || echo "EB MACHINES: ${eb}"
-echo "========================================"
+echo "=================================================================="
 
 ## create repository if does not exists, otherwise update and compile
-mycommand=" mkdir -p DAQ ; cd DAQ ; [ -d H4DAQ ] || git clone git@github.com:cmsromadaq/H4DAQ.git ; cd H4DAQ ; git pull ; python configure.py --noroot ; make -j 4;  "
+mycommand="/bin/bash; cd ${daqhome}; mkdir -p DAQ ; cd DAQ ; [ -d H4DAQ ] || git clone git@github.com:cmsromadaq/H4DAQ.git ; cd H4DAQ ; git pull ; python configure.py --noroot ; make -j 4;  "
 IFS=','
 
 for machine in $dr ; do 
 	
-	mydataro="cd DAQ/H4DAQ; nohup nice -n 0 ./bin/datareadout  -c data/config_${machine}_DR.xml -v ${verbosity} -l /tmp/log_h4daq_datareadout_\$(date +%s).log  > /tmp/log_h4daq_start_dr_${machine}_\$(date +%s).log" 
+	mydataro="/bin/bash; cd ${daqhome}; nohup nice -n1 ./bin/datareadout  -c data/config_${machine}_DR.xml -v ${verbosity} -l ${logdir}/log_h4daq_datareadout_`date +%s`_${daquser}.log  > ${logdir}/log_h4daq_start_dr_${machine}_`date +%s`_${daquser}.log" 
+
 	
 	[ "${dryrun}" == "0" ] || {  echo "$mycommand" ; echo "$mydataro" ; continue; }
 #	[ "${start_dr}" == "0" ] && continue;
 	## compile
-	ssh ${machine}.cern.ch "${mycommand}" 2>&1 | tee /tmp/log_h4daq_update_$machine.log ;
+	[ "${norecompile}" == "1" ] || ssh ${daquser}@${machine}.cern.ch "${mycommand}" 2>&1 | tee /tmp/log_h4daq_update_$machine_${USER}.log ;
 	## launch the daemon
 	echo "-----------------------------"
 	echo "START DATAREADOUT on $machine"
 	echo "-----------------------------"
-	ssh ${machine}.cern.ch "${mydataro}" 2>&1 | tee  /tmp/log_h4daq_start_dr_${machine}_$(date +%s).log ;
+	ssh ${daquser}@${machine}.cern.ch "${mydataro}" 2>&1 | tee  /tmp/log_h4daq_start_dr_{machine}_$(date +%s)_${USER}.log ;
 
 done
 
 for machine in $rc ; do 
 
-	myrc="cd DAQ/H4DAQ; nohup nice -n 0 ./bin/runcontrol  -c data/config_${machine}_RC.xml -v ${verbosity} -l /tmp/log_h4daq_runcontrol_\$(date +%s).log >  /tmp/log_h4daq_start_rc_${machine}_\$(date +%s).log " 
+	myrc="cd ${daqhome}; nohup nice -n1 ./bin/runcontrol  -c data/config_${machine}_RC.xml -v ${verbosity} -l ${logdir}/log_h4daq_runcontrol_`date +%s`_${daquser}.log >  ${logdir}/log_h4daq_start_rc_${machine}_`date +%s`_${daquser}.log " 
 	[ "${dryrun}" == "0" ] || {  echo "$mycommand" ; echo "$mydatarc" ; continue; }
 #	[ "${start_rc}" == "0" ] && continue;
 	## compile
-	ssh ${machine}.cern.ch "${mycommand}" 2>&1 | tee /tmp/log_h4daq_update_$machine.log ;
+	[ "${norecompile}" == "1" ] || ssh ${daquser}@${machine}.cern.ch "${mycommand}" 2>&1 | tee /tmp/log_h4daq_update_$machine_${USER}.log ;
 	## launch the daemon
 	echo "-----------------------------"
 	echo "START RUNCONTROL on $machine"
 	echo "-----------------------------"
-	ssh ${machine}.cern.ch "${myrc}" 2>&1 | tee /tmp/log_h4daq_start_rc_${machine}_$(date +%s).log ;
+	ssh ${daquser}@${machine}.cern.ch "${myrc}" 2>&1 | tee /tmp/log_h4daq_start_rc_${machine}_$(date +%s)_${USER}.log ;
 
 done
 
 for machine in $eb ; do 
 
-	myeb="cd DAQ/H4DAQ; nohup nice -n 0 ./bin/eventbuilder   -c data/config_${machine}_EB.xml -v ${verbosity} -l /tmp/log_h4daq_eventbuilder_\$(date +%s).log >  /tmp/log_h4daq_start_eb_${machine}_\$(date +%s).log " 
+	myeb="cd ${daqhome}; nohup nice -n1 ./bin/eventbuilder   -c data/config_${machine}_EB.xml -v ${verbosity} -l ${logdir}/log_h4daq_eventbuilder_`date +%s`_${daquser}.log >  ${logdir}/log_h4daq_start_eb_${machine}_`date +%s`_${daquser}.log " 
 	[ "${dryrun}" == "0" ] || {  echo "$mycommand" ; echo "$mydatarc" ; continue; }
 #	[ "${start_eb}" == "0" ] && continue;
 	## compile
-	ssh ${machine}.cern.ch "${mycommand}" 2>&1 | tee /tmp/log_h4daq_update_$machine.log ;
+	[ "${norecompile}" == "1" ] || ssh ${daquser}@${machine}.cern.ch "${mycommand}" 2>&1 | tee /tmp/log_h4daq_update_$machine_${USER}.log ;
 	## launch the daemon
 	echo "-----------------------------"
 	echo "START EVENTBUILDER on $machine"
 	echo "-----------------------------"
-	ssh ${machine}.cern.ch "${myrc}" 2>&1 | tee /tmp/log_h4daq_start_rc_${machine}_$(date +%s).log ;
+	ssh ${daquser}@${machine}.cern.ch "${myrc}" 2>&1 | tee /tmp/log_h4daq_start_rc_${machine}_$(date +%s)_${USER}.log ;
 
 done
 
