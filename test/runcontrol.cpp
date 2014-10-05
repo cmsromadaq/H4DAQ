dcc904ae7f3411af3ea11f6bffb52cec0d8d5221
cmsromadaq <cmsdaq@pcethtb3.cern.ch>
2014-10-05 12:53:24 +0200

    add d option to option string

--- test/runcontrol.cpp
+++ test/runcontrol.cpp
@@ -44,7 +44,7 @@ define_handlers();
  };
  
  int long_index =0;
- while ((opt = getopt_long(argc, argv,"c:l:v:", 
+ while ((opt = getopt_long(argc, argv,"c:l:v:d", 
 			   long_options, &long_index )) != -1) {
    switch (opt) {
    case 'c' : configFileName=string(optarg);
