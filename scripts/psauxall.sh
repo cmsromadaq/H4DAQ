6bb7cf4d8924977bbb70ddc30b75b8f6688396eb
Andrea Marini <andrea.marini@cern.ch>
2014-10-05 22:23:11 +0200

    improved psauxall

--- scripts/psauxall.sh
+++ scripts/psauxall.sh
@@ -4,7 +4,7 @@
 # DATA RO 
 for machine in pcethtb1 cms-h4-03 pcethtb2; do 
 	echo "Checking processes on $machine"
-	ssh ${machine}.cern.ch " ps aux | grep 'datareadout\\|runcontrol' "
+	ssh ${machine}.cern.ch " ps aux | grep 'bin/datareadout \\|bin/runcontrol \\|bin/eventbuilder ' | grep -v 'grep' "
 done
 # SLEEP IMPLICIT
 
