2bae1b109b8ca537c243936d77ba3a8b10baa22f
cmsromadaq <cmsdaq@pcethtb3.cern.ch>
2014-10-06 02:06:35 +0200

    Additional debug

--- src/CAEN_V1290.cpp
+++ src/CAEN_V1290.cpp
@@ -345,6 +345,9 @@ int CAEN_V1290::OpWriteTDC(WORD data)
   do {
     status = CAENVME_ReadCycle(handle_,configuration_.baseAddress +  CAEN_V1290_MICROHANDREG ,&rdata, CAEN_V1290_ADDRESSMODE, cvD16);
     time++;
+#ifdef CAENV1290_DEBUG
+    ostringstream s; s << "[CAEN_V1290]::[INFO]::Handshake micro op writing " << rdata << " #" < time << " " << status; 
+#endif
   } while ((rdata != 0x1) && (time < TIMEOUT) );
 
   if ( time == TIMEOUT ) {
@@ -378,6 +381,9 @@ int CAEN_V1290::OpReadTDC(WORD* data)
   do {
     status = CAENVME_ReadCycle(handle_,configuration_.baseAddress +  CAEN_V1290_MICROHANDREG ,&rdata, CAEN_V1290_ADDRESSMODE, cvD16);
     time++;
+#ifdef CAENV1290_DEBUG
+    ostringstream s; s << "[CAEN_V1290]::[INFO]::Handshake micro op reading " << rdata << " #" < time << " " << status; 
+#endif
   } while ((rdata != 0x2) && (time < TIMEOUT) );
 
   if ( time == TIMEOUT ) {
