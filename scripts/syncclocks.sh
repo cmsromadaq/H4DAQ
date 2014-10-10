257a6721e9353732de507c39dc26cb6f32a58f3c
cmsromadaq <cmsdaq@pcethtb3.cern.ch>
2014-10-10 16:20:57 +0200

    fix

--- scripts/syncclocks.sh
+++ scripts/syncclocks.sh
@@ -4,6 +4,6 @@ for machine in pcethtb1 pcethtb2 pcethtb3 cms-h4-03 ; do
     echo "Syncronizing clock on $machine"
     ssh ${machine}.cern.ch "ntpd -q"
     echo "After syncronization:"
-    ssh ${machine}.cern.ch "ntpq -c peers"
+    ssh ${machine}.cern.ch "date"
 done
 
