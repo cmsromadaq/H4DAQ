3484d8fa59fddc06ee91cecdb144dd909f41a8bd
cmsromadaq <cmsdaq@pcethtb3.cern.ch>
2015-04-28 00:39:07 +0200

    fixing kill

--- scripts/killall.sh
+++ scripts/killall.sh
@@ -2,14 +2,14 @@
 
 
 # RC/EB
-for machine in localhost ; do 
+for machine in localhost pcethtb2; do 
 	echo "Terminating runcontrol on $machine"
 	ssh ${machine} "killall runcontrol"
 	echo "Terminating eventbuilder on $machine"
 	ssh ${machine} "killall eventbuilder"
 done
 
-for machine in localhost ; do 
+for machine in localhost pcethtb1; do 
 	echo "Killing runcontrol on $machine"
 	ssh ${machine} "killall -9 runcontrol"
 	echo "Killing eventbuilder on $machine"
