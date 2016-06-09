fb9dbf6beaa0f0c1c768195f4fff45bb5a61e41f
cmsdaq <cmsdaq@cms-h4-06.cern.ch>
2016-06-09 17:57:02 +0200

    improve kill

--- scripts/killall.sh
+++ scripts/killall.sh
@@ -3,7 +3,16 @@
 function kill_machine(){
     machine=$1
     #check if machine is alive
-     if ! [ 'ping -c 1 -w 5 '$machine ' > /dev/null 2>&1' ]; then echo "$machine not reachable"; return -1; fi
+    ping -q -c 2 -w 5 ${machine} > /dev/null
+    if  [ "$?" != 0 ]; then
+        echo "+++ ${machine} is NOT alive +++"
+        return 1
+    fi
+    ssh ${machine} ls > /dev/null
+    if  [ "$?" != 0 ]; then
+        echo "+++ CanNOT connect to ${machine} +++"
+        return 1
+    fi
      echo "Terminating runcontrol on $machine"
      ssh ${machine} "killall -9 runcontrol"
      echo "Terminating readout on $machine"
@@ -12,7 +21,7 @@ function kill_machine(){
      ssh ${machine} "killall -9 eventbuilder"
      echo "Killing datareceiver on $machine"
      ssh ${machine} "killall -9 datareceiver"
-
+     return 0
 }
 # DRO/RC/EB
 for machine in pcethtb1 pcethtb2 pcminn03 cms-h4-03 cms-h4-04 cms-h4-05 pcethtb3; do 
