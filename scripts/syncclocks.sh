179a628380f5eb28a4f7f02365d9bbb158dad1d4
CMSDAQ <cmsdaq@pcethtb1.cern.ch>
2017-10-19 17:24:59 +0200

    update

--- scripts/syncclocks.sh
+++ scripts/syncclocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for machine in pcethtb1 pcethtb2 pcethtb3 cms-h4-03 cms-h4-04 cms-h4-05 pcminn03 pccmsrmtb01; do 
+for machine in pcethtb1 pcethtb2 pcethtb3 cms-h4-03 cms-h4-04 cms-h4-05 pcminn03 pccmsrmtb01 cmsmi5; do 
     echo "Syncronizing clock on $machine"
     ssh root@${machine}.cern.ch -i ~/.ssh/key_for_clock_sync/id_rsa_clock
 done
